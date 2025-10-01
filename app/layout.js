6a7aa234c0bc24e35f8f5dba1c3fad12524c798b
Abstract-code-ops <80575365+Abstract-code-ops@users.noreply.github.com>
2025-10-01 21:27:10 +0300

    Update layout.js to add affiliate script

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import "swiper/css/pagination"
 import 'swiper/css/free-mode';
 import { heebo, maven_pro } from '@/lib/font';
 import I18nProviderWrapper from '@/components/layout/I18nProviderWrapper';
+import Script from 'next/script'
 
 
 export const metadata = {
@@ -21,11 +22,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className={`${maven_pro.variable} ${heebo.variable}`}>
+        <html lang="en" className={`${maven_pro.variable} ${heebo.variable}`}> 
+            <head>
+                <Script id="affiliate-script" strategy="afterInteractive" dangerouslySetInnerHTML={{
+                    __html: `
+                    (function () {
+                        var script = document.createElement("script");
+                        script.async = 1;
+                        script.src = 'https://emrld.ltd/NDYyOTYy.js?t=462962';
+                        document.head.appendChild(script);
+                    })();
+                    `
+                }} data-noptimize="1" data-cfasync="false" data-wpfc-render="false" />
+            </head>
             <body>
-                    <I18nProviderWrapper>
-                            {children}
-                    </I18nProviderWrapper>
+                <I18nProviderWrapper>
+                    {children}
+                </I18nProviderWrapper>
             </body>
         </html>
     )
